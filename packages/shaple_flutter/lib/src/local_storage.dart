c9181f2f6ca3de024b9b46d4521099d151602563
1dennispark <dennis.park@paust.io>
2024-02-16 17:10:59 +0900

    fix(shaple_flutter): remove MigrationLocalStorage and HiveLocalStorage
    BREAKING CHANGE: only used SharedPreferencesLocalStorage

--- packages/shaple_flutter/lib/src/local_storage.dart
+++ packages/shaple_flutter/lib/src/local_storage.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shaple_flutter/shaple_flutter.dart';
 
@@ -20,9 +19,7 @@ const shaplePersistSessionKey = 'SHAPLE_PERSIST_SESSION_KEY';
 ///
 ///   * [ShapleAuth], the instance used to manage authentication
 ///   * [EmptyLocalStorage], used to disable session persistence
-///   * [HiveLocalStorage], that implements Hive as storage method
 ///   * [SharedPreferencesLocalStorage], that implements SharedPreferences as storage method
-///   * [MigrationLocalStorage], to migrate from Hive to SharedPreferences
 abstract class LocalStorage {
   const LocalStorage();
 
@@ -62,62 +59,6 @@ class EmptyLocalStorage extends LocalStorage {
 
   @override
   Future<void> persistSession(persistSessionString) async {}
-}
-
-/// A [LocalStorage] implementation that implements Hive as the
-/// storage method.
-class HiveLocalStorage extends LocalStorage {
-  /// Creates a LocalStorage instance that implements the Hive Database
-  const HiveLocalStorage();
-
-  /// The encryption key used by Hive. If null, the box is not encrypted
-  ///
-  /// This value should not be redefined in runtime, otherwise the user may
-  /// not be fetched correctly
-  ///
-  /// See also:
-  ///
-  ///   * <https://docs.hivedb.dev/#/advanced/encrypted_box?id=encrypted-box>
-  static String? encryptionKey;
-
-  @override
-  Future<void> initialize() async {
-    HiveCipher? encryptionCipher;
-    if (encryptionKey != null) {
-      encryptionCipher = HiveAesCipher(base64Url.decode(encryptionKey!));
-    }
-    await Hive.initFlutter('auth');
-    await Hive.openBox(_hiveBoxName, encryptionCipher: encryptionCipher)
-        .timeout(const Duration(seconds: 1));
-  }
-
-  @override
-  Future<bool> hasAccessToken() {
-    return Future.value(
-      Hive.box(_hiveBoxName).containsKey(
-        shaplePersistSessionKey,
-      ),
-    );
-  }
-
-  @override
-  Future<String?> accessToken() {
-    return Future.value(
-      Hive.box(_hiveBoxName).get(shaplePersistSessionKey) as String?,
-    );
-  }
-
-  @override
-  Future<void> removePersistedSession() {
-    return Hive.box(_hiveBoxName).delete(shaplePersistSessionKey);
-  }
-
-  @override
-  Future<void> persistSession(String persistSessionString) {
-    // Flush after X amount of writes
-    return Hive.box(_hiveBoxName)
-        .put(shaplePersistSessionKey, persistSessionString);
-  }
 }
 
 /// A [LocalStorage] implementation that implements SharedPreferences as the
@@ -170,81 +111,6 @@ class SharedPreferencesLocalStorage extends LocalStorage {
       return web.persistSession(persistSessionKey, persistSessionString);
     }
     return _prefs.setString(persistSessionKey, persistSessionString);
-  }
-}
-
-class MigrationLocalStorage extends LocalStorage {
-  final SharedPreferencesLocalStorage sharedPreferencesLocalStorage;
-  late final HiveLocalStorage hiveLocalStorage;
-
-  MigrationLocalStorage({required String persistSessionKey})
-      : sharedPreferencesLocalStorage =
-            SharedPreferencesLocalStorage(persistSessionKey: persistSessionKey);
-
-  @override
-  Future<void> initialize() async {
-    await Hive.initFlutter('auth');
-    hiveLocalStorage = const HiveLocalStorage();
-    await sharedPreferencesLocalStorage.initialize();
-    try {
-      await migrate();
-    } on TimeoutException {
-      // Do nothing
-    }
-  }
-
-  @visibleForTesting
-  Future<void> migrate() async {
-    // Migrate from Hive to SharedPreferences
-    if (await Hive.boxExists(_hiveBoxName)) {
-      await hiveLocalStorage.initialize();
-
-      final hasHive = await hiveLocalStorage.hasAccessToken();
-      if (hasHive) {
-        final accessToken = await hiveLocalStorage.accessToken();
-        final session =
-            Session.fromJson(jsonDecode(accessToken!)['currentSession']);
-        if (session == null) {
-          return;
-        }
-        await sharedPreferencesLocalStorage
-            .persistSession(jsonEncode(session.toJson()));
-        await hiveLocalStorage.removePersistedSession();
-      }
-      if (Hive.box(_hiveBoxName).isEmpty) {
-        final boxPath = Hive.box(_hiveBoxName).path;
-        await Hive.deleteBoxFromDisk(_hiveBoxName);
-
-        //Delete `auth` folder if it's empty
-        if (!kIsWeb && boxPath != null) {
-          final boxDir = File(boxPath).parent;
-          final dirIsEmpty = await boxDir.list().length == 0;
-          if (dirIsEmpty) {
-            await boxDir.delete();
-          }
-        }
-      }
-    }
-  }
-
-  @override
-  Future<String?> accessToken() {
-    return sharedPreferencesLocalStorage.accessToken();
-  }
-
-  @override
-  Future<bool> hasAccessToken() {
-    return sharedPreferencesLocalStorage.hasAccessToken();
-  }
-
-  @override
-  Future<void> persistSession(String persistSessionString) {
-    return sharedPreferencesLocalStorage.persistSession(persistSessionString);
-  }
-
-  @override
-  Future<void> removePersistedSession() {
-    return sharedPreferencesLocalStorage.removePersistedSession();
   }
 }
 
